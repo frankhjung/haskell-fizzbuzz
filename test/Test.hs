202778c533fbdb0d0694d795b4503a9526918935
Frank Jung <frankhjung@linux.com>
2019-07-01 21:54:38 +1000

    cleanup

--- test/Test.hs
+++ test/Test.hs
@@ -31,36 +31,3 @@ main = hspec $
       property $ forAll genMod3and5 $ \n -> fizzbuzz n `shouldBe` "fizzbuzz"
     it "not modulo 3 or 5 == n" $
       property $ forAll genNotFizzBuzz $ \n -> fizzbuzz n `shouldBe` show n
-
-  -- describe "return number if not multiples of 3 or 5" $ do
-  --   context "1 = 1?" $
-  --     it "returns 1" $
-  --       fizzbuzz 1 `shouldBe` "1"
-  --   context "8 = 8?" $
-  --     it "returns 8" $
-  --       fizzbuzz 8 `shouldBe` "8"
-
-  -- describe "fizz are multiples of 3" $ do
-  --   context "3 = fizz?" $
-  --     it "returns fizz" $
-  --       fizzbuzz 3 `shouldBe` "fizz"
-  --   context "9 = fizz?" $
-  --     it "returns fizz" $
-  --       fizzbuzz 9 `shouldBe` "fizz"
-
-  -- describe "buzz are multiples of 5" $ do
-  --   context "5 = buzz?" $
-  --     it "returns buzz" $
-  --       fizzbuzz 5 `shouldBe` "buzz"
-  --   context "10 = buzz?" $
-  --     it "returns buzz" $
-  --       fizzbuzz 10 `shouldBe` "buzz"
-
-  -- describe "fizzbuzz are multiples of 3 and 5" $ do
-  --   context "15 = fizzbuzz?" $
-  --     it "returns fizzbuzz" $
-  --       fizzbuzz 15 `shouldBe` "fizzbuzz"
-  --   context "30 = fizzbuzz?" $
-  --     it "returns fizzbuzz" $
-  --       fizzbuzz 30 `shouldBe` "fizzbuzz"
-
