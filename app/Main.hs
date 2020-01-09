c06a401bd42bcc7d54574983d272bd1347dba57c
Frank Jung <frankhjung@linux.com>
2020-01-10 10:04:51 +1100

    using semigroup resonance

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,16 @@
 module Main where
 
-import           FizzBuzz           (fizzbuzz)
+import           FizzBuzz           (fizzbuzz, fizzbuzz')
 import           System.Environment
 
 play :: Int -> [String]
 play n = map fizzbuzz [1..n]
 
+play' :: Int -> [String]
+play' = fizzbuzz'
+
 main :: IO ()
 main = do
   n <- fmap (read . head) getArgs :: IO Int
-  mapM_ putStrLn (play n)
+  mapM_ putStrLn (play' n)
 
--- src/FizzBuzz.hs
+++ src/FizzBuzz.hs
@@ -12,9 +12,12 @@
 
 -}
 
-module FizzBuzz (fizzbuzz) where
+module FizzBuzz (fizzbuzz, fizzbuzz') where
 
--- | Given n returns fizzbuzz returns:
+import           Data.List  (zipWith)
+import           Data.Maybe (fromMaybe)
+
+-- | Given n returns fizzbuzzes.
 --
 --    * fizz if modulo 3
 --    * buzz if modulo 5
@@ -23,7 +26,26 @@ module FizzBuzz (fizzbuzz) where
 fizzbuzz :: Int -> String
 fizzbuzz n = (fizz . buzz) id (show n)
   where
-    fizz  | n `mod` 3 == 0 = \x -> const ("fizz" ++ x "")
-          | otherwise = id
-    buzz  | n `mod` 5 == 0 = \x -> const ("buzz" ++ x "")
-          | otherwise = id
+    fizz | n `mod` 3 == 0 = \x -> const ("fizz" ++ x "")
+         | otherwise = id
+    buzz | n `mod` 5 == 0 = \x -> const ("buzz" ++ x "")
+         | otherwise = id
+
+-- | Semigroup resonance FizzBuzz by Mark Seemann.
+-- This "FizzBuzz kata uses the fundamental concepts of catamorphism, semigroup
+-- and monoid. No if-then-else instructions or pattern matching is required.
+-- Instead, we use the string concatenation semigroup to enable resonance between
+-- two pulses, and the maybe catamorphism to combine with the list of numbers."
+-- Source: https://blog.ploeh.dk/2019/12/30/semigroup-resonance-fizzbuzz/
+fizzbuzz' :: Int -> [String]
+fizzbuzz' n = zipWith fromMaybe numbers fizzBuzzes
+  where
+    numbers :: [String]
+    numbers = show <$> [1..n]
+    fizzes :: [Maybe String]
+    fizzes = cycle [Nothing, Nothing, Just "Fizz"]
+    buzzes :: [Maybe String]
+    buzzes = cycle [Nothing, Nothing, Nothing, Nothing, Just "Buzz"]
+    fizzBuzzes :: [Maybe String]
+    fizzBuzzes = zipWith (<>) fizzes buzzes
+
